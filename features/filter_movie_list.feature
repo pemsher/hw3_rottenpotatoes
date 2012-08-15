c366b5735ebb3a362d29b5cf07e0289b87b46ef4
Pemba Sherpa <pemsher@gmail.com>
2012-08-15 11:26:22 -0700

    HW3-part1-2

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,26 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
+  # When I check "ratings_PG"
+  # And I check "ratings_R"
   # enter step(s) to uncheck all other checkboxes
   # enter step to "submit" the search form on the homepage
   # enter step(s) to ensure that PG and R movies are visible
   # enter step(s) to ensure that other movies are not visible
+  When I check the following ratings: PG,R
+  And I uncheck the following ratings: PG-13,G
+  And I press "ratings_submit"
+  Then I should see "Amelie"
+  And I should see "The Incredibles"
 
 Scenario: no ratings selected
   # see assignment
+  When I press "ratings_submit"
+  Then I should see none of the movies
 
 Scenario: all ratings selected
   # see assignment
+  When I check the following ratings: PG,R,G,PG-13
+  And I press "ratings_submit"
+  Then I should see all of the movies
+
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -3,9 +3,10 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    # you should arrange to add that movie to the database here.a
+    Movie.create(movie)
   end
-  flunk "Unimplemented"
+#flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +26,18 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(",").each do |rating|
+    if uncheck == 'un'
+      step "I uncheck \"ratings_#{rating}\""
+    else
+       step "I check \"ratings_#{rating}\""
+    end
+  end
+end
+Then /I should see (all|none) of the movies/ do |all|
+  if all == 'all'
+    page.all('table#movies tr').count.should == 11
+  else
+    page.all('table#movies tr').count.should == 1
+  end
 end
